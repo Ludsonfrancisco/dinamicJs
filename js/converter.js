c5f144019d068433c57571b3f6ba72956e07e2e3
Ludson Francisco <ludsonfrancisco1010@gmail.com>
2025-02-06 08:38:52 -0300

    Update OC converter.js

--- js/converter.js
+++ js/converter.js
@@ -87,13 +87,20 @@ document.getElementById('button').addEventListener('click', () => {
 
 
 
-                    rowObject = removeItem(rowObject, 'ID de Armário', 'ESSEA_G1M10')
-                    rowObject = removeItem(rowObject, 'ID de Armário', 'ESSEA_G1M11')
-                    rowObject = removeItem(rowObject, 'ID de Armário', 'ESSEA_G1M03')
-                    rowObject = removeItem(rowObject, 'ID de Armário', 'ESSEA_G1N08')
-                    rowObject = removeItem(rowObject, 'ID de Armário', 'ESSEA_G1N06')
-                    rowObject = removeItem(rowObject, 'ID de Armário', 'ESSEA_G1N09')
-                    rowObject = removeItem(rowObject, 'ID de Armário', 'ESSEA_G1I02')
+                    //rowObject = removeItem(rowObject, 'ID de Armário', 'ESSEA_G1M10')
+                    //rowObject = removeItem(rowObject, 'ID de Armário', 'ESSEA_G1M11')
+                    //rowObject = removeItem(rowObject, 'ID de Armário', 'ESSEA_G1M03')
+                    //rowObject = removeItem(rowObject, 'ID de Armário', 'ESSEA_G1N08')
+                    //rowObject = removeItem(rowObject, 'ID de Armário', 'ESSEA_G1N06')
+                    //rowObject = removeItem(rowObject, 'ID de Armário', 'ESSEA_G1N09')
+                    //rowObject = removeItem(rowObject, 'ID de Armário', 'ESSEA_G1I02')
+
+                    rowObject = removeItem(rowObject, 'ID de Armário', 'ESSEA_G1M04')
+                    rowObject = removeItem(rowObject, 'ID de Armário', 'ESSEA_G1M05')
+                    rowObject = removeItem(rowObject, 'ID de Armário', 'ESSEA_G1N02')
+                    rowObject = removeItem(rowObject, 'ID de Armário', 'ESSEA_G1N07')
+
+                    
                     rowObject = removeItem(rowObject, 'Detalhe da Atividade', 'PREV/BANDA LARGA')
                     rowObject = removeItem(rowObject, 'Detalhe da Atividade', 'PREV/PÓS CONTATO')
                     rowObject = removeItem(rowObject, 'Detalhe da Atividade', 'Prev/Pós Contato')
